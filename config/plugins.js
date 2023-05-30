3e3b2f376554675b33ffb980a162f0c5284ee4e7
Manohar <itsmanoharkumar@gmail.com>
2023-05-30 10:39:28 +0530

    really ignore ignored worsd

--- config/plugins.js
+++ config/plugins.js
@@ -1,4 +1,14 @@
 module.exports = ({env}) => ({
+  "vercel-deploy": {
+    enabled: true,
+    config: {
+      deployHook: env("VERCEL_DEPLOY_HOOK"),
+      apiToken: env("VERCEL_API_TOKEN"),
+      appFilter: env("VERCEL_APP_FILTER"),
+      teamFilter: env('VERCEL_TEAM_FILTER'),
+      roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
+    }
+  },
   upload: {
     config: {
       provider: process.env.NODE_ENV !== 'production'?"local": "strapi-provider-upload-azure-storage",
